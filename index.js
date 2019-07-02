79e3b45bbe921d00d703093a145c8ad4374098cb
chumaidi <chumaidihxc@gmail.com>
2019-07-02 09:26:16 +0700

    adding variable

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+  var moment = require('moment');
+moment().format();  
   const yearBorn = moment().format("1990");
   const yearNow = moment().year ();
 
   setTimeout(() => {
 console.log(yearNow - yearBorn);
-  }2000);
-  )
+  },2000);
+  
